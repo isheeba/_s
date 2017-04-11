521b873c28027c67b1262d82f1b0ef224af4b2d5
Saurabh Shukla <saurabh@yapapaya.com>
2017-04-11 10:06:54 +0530

    Adds slideshow functionality

--- inc/item-slideshow.php
+++ inc/item-slideshow.php
@@ -1,8 +1,27 @@
 <?php
 
-function saurabh_theme_slideshow_scripts(){
-	wp_register_script('bxslider', STYLESHEETPATH . 'js/bx-slider.js');
-	wp_enqueue_script('bxslider');
+/**
+ * Enqueues bx-slider's styles and scripts
+ */
+function saurabh_theme_slideshow_scripts() {
+
+	// check if it is a single post of post type handmedown
+	if ( is_singular( 'handmedown' ) ) {
+
+		wp_register_script( 'jquery-bxslider', get_stylesheet_directory_uri() . 'js/jquery.bxslider.js', array( 'jquery' ), '' );
+
+		wp_register_script( 'saurabh-slideshow', get_stylesheet_directory_uri() . 'js/slideshow.js', array( 'jquery-bxslider' ), '', true );
+
+		wp_enqueue_script( 'saurabh-slideshow' );
+
+
+		wp_register_style( 'jquery-bxslider', get_stylesheet_directory_uri() . 'css/jquery.bxslider.css' );
+
+		wp_enqueue_style( 'jquery-bxslider' );
+	}
 }
 
 add_action( 'wp_enqueue_scripts', 'saurabh_theme_slideshow_scripts' );
+
+
+
--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,5 @@
 jQuery( 'document' ).ready( function( $ ) {
-	
-	$()
+	$('bxslider').bxslider({
+		'pager' : false
+	});
 } );
