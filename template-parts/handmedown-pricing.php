60fa34bcd708469c4947f7811efbbae6c29459ce
Saurabh Shukla <saurabh@yapapaya.com>
2017-04-13 09:09:24 +0530

    Adds price formatting

--- template-parts/handmedown-pricing.php
+++ template-parts/handmedown-pricing.php
@@ -1,14 +1,20 @@
 <?php
-// 100000
 $price = get_post_meta( $post_id, 'saurabh_theme_price', true );
+
+// formats price with 2 decimal places
+$price_decimal = number_format( $price, 2 );
+
+// formats price according to the locale
+$price_localised = number_format_i18n( $price, 2 );
 ?>
 <section class="hmd-pricing">
 		<div itemprop="offers" itemscope itemtype="http://schema.org/Offer" class="hmd-offer">
-				<span itemprop="priceCurrency" content="INR" class="hmd-currency">₹</span><span
-						itemprop="price" content="100000.00" class="hmd-price"><?php number_format_i18n( $price, 2 ); ?></span>
+				<span itemprop="priceCurrency" content="INR" class="hmd-currency">₹</span>
+				<span itemprop="price" content="<?php echo $price_decimal ?>" class="hmd-price">
+					<?php echo $price_localised ?>
+				</span>
 		</div>
 		<form class="hmd-cta-form">
 				<input type="submit" name="hmd-submit" value="">
 		</form>
 </section>
-
