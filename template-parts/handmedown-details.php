993f0ced057edb955ad69c859810a685d26e1cab
Sheeba Abraham <sheeba@yapapaya.com>
2017-04-21 08:44:05 +0530

    Added field availability in details

--- template-parts/handmedown-details.php
+++ template-parts/handmedown-details.php
@@ -31,18 +31,58 @@ $accessories = get_post_meta( $post_id, 'sheeba_accessories', true );
 $warranty_mon = get_post_meta( $post_id, 'sheeba_warranty_mon', true );
 $warranty_yr = get_post_meta( $post_id, 'sheeba_warranty_yr', true );
 
+// Get all the available meta values for this post id.
+$availability = get_post_meta($post_id,'sheeba_availability');
+
 ?>
 <section class="hmd-details">
-	<div itemprop="property" itemscope itemtype="http://meta.schema.org/Property" class="hmd-meta">
+	<dl itemprop="property" itemscope itemtype="http://meta.schema.org/Property" class="hmd-meta">
+
+		// Display the Age of the item.
 		<span itemprop="typicalAgeRange" class="hmd-age-range">
-			<?php echo "$yrs_from - $yrs_to"; ?>
+			<dt>Age Range</dt>
+			<dd><?php echo "$yrs_from - $yrs_to"; ?></dd>
 		</span>
-		<span itemprop="brand" class="hmd-brand"> <?php echo $brand; ?> </span>
-		<span itemprop="model" class="hmd-model"> <?php echo $model; ?> </span>
-		<span itemprop="dimension" class="hmd-dimension"> <?php echo $dimension; ?> </span>
-		<span itemprop="accessories" class="hmd-accessories"> <?php echo $accessories; ?> </span>
+
+		// Display the brand name of the item
+		<span itemprop="brand" class="hmd-brand">
+			<dt>Brand</dt>
+			<dd><?php echo $brand; ?></dd>
+		</span>
+
+		// Display the Model of the item
+		<span itemprop="model" class="hmd-model">
+			<dt>Model</dt>
+			<dd><?php echo $model; ?> </dd>
+		</span>
+
+		// Display the Dimensions  of the item	.
+		<span itemprop="dimension" class="hmd-dimension">
+			<dt>Dimensions</dt>
+			<dd> <?php echo $dimension; ?> </dd>
+		</span>
+
+		// Display Accessories related to the item.
+		<span itemprop="accessories" class="hmd-accessories">
+			<dt>Accessories</dt>
+			<dd> <?php echo $accessories; ?> </dd>
+		</span>
+
+		// Display leftover warranty in MM,YY format.
 		<span itemprop="warranty" class="hmd-warranty">
-			<?php echo "$warranty_mon, $warranty_yr"; ?>
+			<dt>Warranty</dt>
+			<dd> <?php echo "$warranty_mon, $warranty_yr"; ?> </dd>
 		</span>
-	</div>
+
+		// Display the seller time availability.
+		<span itemprop=" " class="hmd-availability">
+			<dt>Availability</dt>
+			<dd>
+				<?php
+				foreach($availability as $day=>$time_avail)
+					echo $day . ' : ' . $time_avail[0] . '<br/>';
+				?>
+			</dd>
+		</span>
+	</dl>
 </section>
